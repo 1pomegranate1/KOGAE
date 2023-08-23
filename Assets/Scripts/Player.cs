90cc2d92e28411798949268aff5f2beaa54332cb
1pomegranate1 <0326zxc9090@gmail.com>
2023-08-23 18:14:14 +0900

    1
    더블클릭, 대쉬 추가

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,5 +1,8 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Timers;
 using Unity.VisualScripting;
 using UnityEngine;
 
@@ -13,10 +16,13 @@ public class Player : MonoBehaviour
 {
     [Header("컴포넌트")]
     [SerializeField] Rigidbody2D rb;
+    bool inputX_p, inputX_m, inputY_p, inputY_m;
     Camera mainCamera;
 
     [Header("입력")]
     List<InputTimeLine> inputs = new List<InputTimeLine>();
+    Dictionary<KeyCode, System.Action> doubleClickAction = new Dictionary<KeyCode, System.Action>();
+    [SerializeField] float doubleClickTime;
 
     [Header("이동")]
     Vector2 inputVec;
@@ -30,16 +36,19 @@ public class Player : MonoBehaviour
     public int ableJumpCount;
     [SerializeField] int maxJumpCount;
     [SerializeField] float jumpForce;
-    [SerializeField] Vector2 footSize,footOffset;
+    [SerializeField] Vector2 footSize, footOffset;
+    float jumpDelay;
+    [SerializeField] float minJumpDelay;
 
-    [Header("데쉬")]
+    [Header("대쉬")]
     bool isDashing;
-    float dashForce;
+    [SerializeField] float dashForce;
 
     // Start is called before the first frame update
     void Start()
     {
         mainCamera = Camera.main;
+        DoubleClickSetup();
     }
 
     void FixedUpdate()
@@ -47,45 +56,195 @@ public class Player : MonoBehaviour
         FixedMove();
         Landing();
     }
+    private void LateUpdate()
+    {
+        DoubleClickUpdate();
+    }
     void Update()
     {
         InputUpdate();
         Look();
-        Jump();
+        Jump(true);
+    }
+    void DoubleClickSetup()
+    {
+        doubleClickAction.Add(KeyCode.W, () => { });
+        doubleClickAction.Add(KeyCode.A, () => Dash(false));
+        doubleClickAction.Add(KeyCode.S, () => { });
+        doubleClickAction.Add(KeyCode.D, () => Dash(true));
+    }
+    bool DoubleClickCheck(KeyCode key)
+    {
+        bool isDoubleClick = false;
+        for (int i = 0; i < inputs.Count; i++)
+        {
+            if (inputs[i].key == key)
+            {
+                isDoubleClick = true;
+                inputs.RemoveAt(i);
+                break;
+            }
+        }
+        if (isDoubleClick) {
+            doubleClickAction[key].Invoke();
+        }
+        else
+        {
+            inputs.Add(new InputTimeLine { key = key, leftTime = doubleClickTime });
+        }
+        return isDoubleClick;
     }
     void InputUpdate()
     {
         inputVec = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
-        for (int i = inputs.Count; i < 0; i--)
+        if (inputVec.x != 0)
         {
-            if(inputs[i].leftTime > 0 )//-= Time.deltaTime;{
+            if (inputVec.x < 0)
             {
-                inputs[i] = new InputTimeLine { key = inputs[i].key, leftTime = inputs[i].leftTime - Time.deltaTime };
+                if (inputX_m == false)
+                {
+                    // 더블 클릭인지 확인하고 이미 눌렀으면 더블 클릭 발동하는 함수 제작하셈 
+                    DoubleClickCheck(KeyCode.A);
+                }
+                else
+                    UiManager.PlayerInputUI(-1, 1, -1, 0);
+                inputX_m = true;
             }
             else
             {
-                inputs.RemoveAt(i);
+                if (inputX_p == false)
+                {
+                    DoubleClickCheck(KeyCode.D);
+                }
+                else
+                    UiManager.PlayerInputUI(-1, 0, -1, 1);
+                inputX_p = true;
             }
+        }
+        else
+        {
+            UiManager.PlayerInputUI(-1, 0, -1, 0);
+            inputX_p = false;
+            inputX_m = false;
+        }
+        if (inputVec.y != 0)
+        {
+            if (inputVec.y < 0)
+            {
+                if (inputY_m == false)
+                {
+                    DoubleClickCheck(KeyCode.S);
+                }
+                else
+                    UiManager.PlayerInputUI(0, -1, 1, -1);
+                inputY_m = true;
+            }
+            else
+            {
+                if (inputY_p == false)
+                {
+                    Jump(false);
+                    DoubleClickCheck(KeyCode.W);
+                }
+                else 
+                    UiManager.PlayerInputUI(1, -1, 0, -1);
+                inputY_p = true;
+            }
+        }
+        else
+        {
+            UiManager.PlayerInputUI(0, -1, 0, -1);
+            inputY_p = false;
+            inputY_m = false;
+        }
+    }
+    void DoubleClickUpdate() {
+        if (inputs.Count > 0)
+        {
+            for (int i = inputs.Count - 1; i >= 0; i--)
+            {
+                if (inputs[i].leftTime > 0)//-= Time.deltaTime;{
+                {
+                    inputs[i] = new InputTimeLine { key = inputs[i].key, leftTime = inputs[i].leftTime - Time.deltaTime };
 
+                    switch (inputs[i].key)
+                    {
+                        case KeyCode.W:
+                            UiManager.PlayerInputUI(2, -1, -1, -1);
+                            break;
+                        case KeyCode.A:
+                            UiManager.PlayerInputUI(-1, 2, -1, -1);
+                            break;
+                        case KeyCode.S:
+                            UiManager.PlayerInputUI(-1, -1, 2, -1);
+                            break;
+                        case KeyCode.D:
+                            UiManager.PlayerInputUI(-1, -1, -1, 2);
+                            break;
+                    }
+                }
+                else
+                {
+                    switch (inputs[i].key)
+                    {
+                        case KeyCode.W:
+                            UiManager.PlayerInputUI(0, -1, -1, -1);
+                            break;
+                        case KeyCode.A:
+                            UiManager.PlayerInputUI(-1, 0, -1, -1);
+                            break;
+                        case KeyCode.S:
+                            UiManager.PlayerInputUI(-1, -1, 0, -1);
+                            break;
+                        case KeyCode.D:
+                            UiManager.PlayerInputUI(-1, -1, -1, 0);
+                            break;
+                    }
+                    inputs.RemoveAt(i);
+                }
+            }
         }
     }
     void FixedMove()
     {
+        if (isDashing)
+        {
+            rb.velocity = new Vector2(rb.velocity.x * 0.95f, rb.velocity.y);
+            if (rb.velocity.x >= -5f && rb.velocity.x <= 5f)
+            {
+                isDashing = false;
+            }
+            return;
+        }
         float velocityY = rb.velocity.y;
         if (inputVec.x != 0)
         {
             rb.velocity = new Vector2(
                 inputVec.x * speed * Time.fixedDeltaTime,
-                velocityY   
+                velocityY
                 );
         }
         else
             rb.velocity = Vector2.up * velocityY;
     }
-    void Jump()
+    void Jump(bool stateCheck)
     {
-        if (inputVec.y > 0 && ableJumpCount > 0)
+        if (stateCheck)
         {
+            if (jumpDelay < 0)
+            {
+                jumpDelay = 0;
+            }
+            else
+            {
+                jumpDelay -= Time.deltaTime;
+            }
+            return;
+        }
+        if (ableJumpCount > 0)
+        {
+            if (ableJumpCount == maxJumpCount)
+                jumpDelay = minJumpDelay;
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
             ableJumpCount--;
         }
@@ -96,12 +255,21 @@ public class Player : MonoBehaviour
         angle = lookVec.ToDegree() - 90;
         UiManager.PlayerAngleUI(angle);
     }
+    public void Dash(bool isRight)
+    {
+        float velocityY = rb.velocity.y;
+        print("Dash-" + (isRight ? "right" : "left"));
+        rb.velocity = new Vector2(dashForce * (isRight ? 1 : -1), velocityY);
+        isDashing = true;
+    }
     private void OnDrawGizmos()
     {
         Gizmos.DrawWireSphere(rb.position + lookVec, 0.1f);
     }
     void Landing() // 착지를 검사하고 땅에있다면 점프 횟수를 추가하는 메소드
     {
+        if (jumpDelay > 0)
+            return;
         Collider2D[] underHits = Physics2D.OverlapCapsuleAll(rb.position + footOffset, footSize, CapsuleDirection2D.Horizontal, 0);
         if (underHits.Length > 0) {
             bool onGround = false;
--- Assets/Scripts/UiManager.cs
+++ Assets/Scripts/UiManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UiManager : MonoBehaviour
 {
@@ -9,6 +10,9 @@ public class UiManager : MonoBehaviour
 
     [SerializeField] TextMeshProUGUI playerAngleText;
 
+    [Header("입력 디버그")]
+    [SerializeField] Image wKey, aKey, sKey, dKey;
+    Color yellowGreen = new Color(0.52f, 0.82f, 0);
     private void Awake()
     {
         instance = this;
@@ -18,5 +22,66 @@ public class UiManager : MonoBehaviour
     void _PlayerAngleUI(float angle)
     {
         playerAngleText.text = angle.ToString("f1");
+    }
+
+    static public void PlayerInputUI(int w, int a, int s, int d) => instance._PlayerInputUI(w, a, s, d);
+    void _PlayerInputUI(int w, int a, int s, int d)
+    {
+        switch (w)
+        {
+            case -1:
+                break;
+            case 0:
+                wKey.color = Color.white;
+                break;
+            case 1:
+                wKey.color = Color.green;
+                break;
+            case 2:
+                wKey.color = Color.yellow;
+                break;
+        }
+        switch (a)
+        {
+            case -1:
+                break;
+            case 0:
+                aKey.color = Color.white;
+                break;
+            case 1:
+                aKey.color = Color.green;
+                break;
+            case 2:
+                aKey.color = Color.yellow;
+                break;
+        }
+        switch (s)
+        {
+            case -1:
+                break;
+            case 0:
+                sKey.color = Color.white;
+                break;
+            case 1:
+                sKey.color = Color.green;
+                break;
+            case 2:
+                sKey.color = Color.yellow;
+                break;
+        }
+        switch (d)
+        {
+            case -1:
+                break;
+            case 0:
+                dKey.color = Color.white;
+                break;
+            case 1:
+                dKey.color = Color.green;
+                break;
+            case 2:
+                dKey.color = Color.yellow;
+                break;
+        }
     }
 }
